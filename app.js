ebcb49bdbb5242fa73f657772cab14bddc095d16
unknown <ajinkyak1510@gmail.com>
2023-06-21 12:27:51 +0530

    use of let and adding header and footer ejs

--- app.js
+++ app.js
@@ -7,26 +7,44 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-var items = [];
+let items = ["Buy", "Cook", "Eat"];
+let work_item = [];
 
 app.get("/",(req,res)=>{
     const today = new Date();
     const currDay = today.getDay();
     // const dayArray = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var options = {
+    let options = {
         weekday:"long",
         day:"numeric",
         month:"long"
     }
-    var day = today.toLocaleDateString("en-US",options);
-    res.render("list",{kindOfDay:day,newListItem:items});
+    let day = today.toLocaleDateString("en-US",options);
+    res.render("list",{listTitle:day,newListItem:items});
 })
 
 
+app.get("/work",(req,res)=>{
+    res.render("list",{listTitle:"Work",newListItem:work_item});
+})
+
+
+app.post("/work",(req,res)=>{
+    
+})
+
 app.post("/",(req,res)=>{
-    var item = req.body.newitem;
-    items.push(item);
-    res.redirect("/");
+    console.log(req.body);
+    let item = req.body.newitem;
+    if(req.body.list === "Work"){
+        work_item.push(item);
+        res.redirect("/work");
+    }
+    else{
+        
+        items.push(item);
+        res.redirect("/");
+    }
 })
 
 
